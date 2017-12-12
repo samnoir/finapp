629347f851b59ef9598e839c61c044ef71b5339e
Sam Noir <sreeni.nair@gmail.com>
2017-12-12 06:21:19 -0500

    - Do makemigrations and migrate

--- finance/models.py
+++ finance/models.py
@@ -9,32 +9,6 @@ class FinBase(models.Model):
     class Meta:
             abstract = True
 
-class Account(FinBase):
-        name = models.CharField(max_length=64)
-        transactions = models.ManyToManyField(
-                            Transaction,
-                            through="Split"
-                        )
-
-        
-        def __str__(self):
-            return self.name
-
-class Split(FinBase):
-        CREDIT = 'C'
-        DEBIT = 'D'
-        OP_CHOICES = (
-            (CREDIT, "CREDIT"),
-            (DEBIT, "DEBIT"),
-        )
-        amount = models.FloatField()
-        op = models.CharField(choice=OP_CHOICES, max_length=1)
-        account = models.ForeignKey(Account, on_delete=models.CASCADE)
-        transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE)
-        is_reconciled = models.BooleanField(default=False)
-
-    def __str__(self):
-        return "{0} {1} {2}".format(self.op, self.account, self.amount)
 
 class Transaction(FinBase):
         INCOME = 'INC'
@@ -44,13 +18,42 @@ class Transaction(FinBase):
             (EXPENSE, "Expense"),
         )
         tdate = models.DateField()
-        ttype = models.CharField(choice=TTYPE_CHOICES, max_length=3, default=EXPENSE)
+        ttype = models.CharField(choices=TTYPE_CHOICES, max_length=3,
+                                 default=EXPENSE)
         other_party = models.CharField(max_length=50)
         amount = models.FloatField()
         categories = models.CharField(max_length=128)
         is_reconciled = models.BooleanField(default=False)
-        recurrent = models.BooleanField(default=False) 
-        #This needs to be captured using RRules
+        recurrent = models.BooleanField(default=False)
+        # This needs to be captured using RRules
 
-    def __str__(self):
-        return "{0} {1}->{2} on {3}".format(amount, accounts, other_party, tdate)
+        def __str__(self):
+            return "{0} to {1} on {2}".format(self.amount,
+                                              self.other_party,
+                                              self.tdate)
+
+
+class Account(FinBase):
+        name = models.CharField(max_length=64)
+        transactions = models.ManyToManyField(
+                            Transaction,
+                            through="Split"
+                        )
+
+        def __str__(self):
+            return self.name
+class Split(FinBase):
+        CREDIT = 'C'
+        DEBIT = 'D'
+        OP_CHOICES = (
+            (CREDIT, "CREDIT"),
+            (DEBIT, "DEBIT"),
+        )
+        amount = models.FloatField()
+        op = models.CharField(choices=OP_CHOICES, max_length=1)
+        account = models.ForeignKey(Account, on_delete=models.CASCADE)
+        transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE)
+        is_reconciled = models.BooleanField(default=False)
+
+        def __str__(self):
+            return "{0} {1} {2}".format(self.op, self.account, self.amount)
--- mysite/settings.py
+++ mysite/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    #'finance.apps.config',
+    'finance.apps.FinanceConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #'finance',
 ]
 
 MIDDLEWARE = [
